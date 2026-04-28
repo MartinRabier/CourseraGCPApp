02e9b1dd19214bf49bbf5738182a7982f91046ac
MartinRabier <157110077+MartinRabier@users.noreply.github.com>
2026-04-28 18:25:48 +0200

    Update greeting message in hello route

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
   """return a friendly HTTP greeting"""
   print("I am inside hello world")
-  return 'Hello world ! (CD working now :o)'
+  return 'Hello world ! (CD plz work now)'
 
 @app.route('/echo/<name>')
 def echo(name):
